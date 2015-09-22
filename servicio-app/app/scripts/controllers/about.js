dfc77aa6772f2dcf21e7e499856194aa53e038ff
Chunkzer <cruink.90@gmail.com>
2015-09-21 23:31:30 -0700

    Scaffolding

---
+++ servicio-app/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name servicioAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the servicioAppApp
+ */
+angular.module('servicioAppApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ servicio-app/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name servicioAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the servicioAppApp
+ */
+angular.module('servicioAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
